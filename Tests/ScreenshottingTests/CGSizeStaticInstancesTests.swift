e25970b5cf2d9a278ca996766c3392216e7e9d75
Igor Malyarov <igor1309@gmail.com>
2022-09-10 15:09:42 +0300

    extract tests for static instances

---
+++ Tests/ScreenshottingTests/CGSizeStaticInstancesTests.swift
@@ -0,0 +1,81 @@
+//
+//  CGSizeStaticInstancesTests.swift
+//  ScreenshottingTests
+//
+//  Created by Igor Malyarov on 10.09.2022.
+//
+
+import Screenshotting
+import XCTest
+
+final class CGSizeStaticInstancesTests: XCTestCase {
+    
+    func test_CGSize_width() {
+        /// `4.7"` (375 x 667 points @2x)
+        /// iPhone SE (2nd Gen), iPhone 8, iPhone 7, iPhone 6S
+        XCTAssertEqual(CGSize.iPhoneSE2nd.width, 375)
+        
+        /// `5.4"` (375 x 812 points @3x)
+        /// iPhone 13 mini, iPhone 12 mini
+        XCTAssertEqual(CGSize.iPhone13mini.width, 375)
+        
+        /// `5.5"` (414 x 736 points @3x)
+        /// iPhone 8 Plus, iPhone 7 Plus, iPhone 6S Plus
+        XCTAssertEqual(CGSize.iPhone8Plus.width, 414)
+        
+        /// `5.8"` (375 x 812 points @3x)
+        /// iPhone 11 Pro, iPhone XS, iPhone X
+        XCTAssertEqual(CGSize.iPhoneX.width, 375)
+        
+        /// `6.1"` (414 x 896 points @2x)
+        /// iPhone 11, iPhone XR
+        XCTAssertEqual(CGSize.iPhone11.width, 414)
+        
+        /// `6.1"` (390 x 844 points @3x)
+        /// iPhone 13 Pro, iPhone 13, iPhone 12 Pro, iPhone 12
+        XCTAssertEqual(CGSize.iPhone13Pro.width, 390)
+        
+        /// `6.5"` (414 x 896 points @3x)
+        /// iPhone 11 Pro Max, iPhone XS Max
+        XCTAssertEqual(CGSize.iPhoneXSMax.width, 414)
+        
+        /// `6.7"` (428 x 926 points @3x)
+        /// iPhone 13 Pro Max, iPhone 12 Pro Max
+        XCTAssertEqual(CGSize.iPhone13ProMax.width, 428)
+    }
+    
+    func test_CGSize_height() {
+        /// `4.7"` (375 x 667 points @2x)
+        /// iPhone SE (2nd Gen), iPhone 8, iPhone 7, iPhone 6S
+        XCTAssertEqual(CGSize.iPhoneSE2nd.height, 667)
+        
+        /// `5.4"` (375 x 812 points @3x)
+        /// iPhone 13 mini, iPhone 12 mini
+        XCTAssertEqual(CGSize.iPhone13mini.height, 812)
+        
+        /// `5.5"` (414 x 736 points @3x)
+        /// iPhone 8 Plus, iPhone 7 Plus, iPhone 6S Plus
+        XCTAssertEqual(CGSize.iPhone8Plus.height, 736)
+        
+        /// `5.8"` (375 x 812 points @3x)
+        /// iPhone 11 Pro, iPhone XS, iPhone X
+        XCTAssertEqual(CGSize.iPhoneX.height, 812)
+        
+        /// `6.1"` (414 x 896 points @2x)
+        /// iPhone 11, iPhone XR
+        XCTAssertEqual(CGSize.iPhone11.height, 896)
+        
+        /// `6.1"` (390 x 844 points @3x)
+        /// iPhone 13 Pro, iPhone 13, iPhone 12 Pro, iPhone 12
+        XCTAssertEqual(CGSize.iPhone13Pro.height, 844)
+        
+        /// `6.5"` (414 x 896 points @3x)
+        /// iPhone 11 Pro Max, iPhone XS Max
+        XCTAssertEqual(CGSize.iPhoneXSMax.height, 896)
+        
+        /// `6.7"` (428 x 926 points @3x)
+        /// iPhone 13 Pro Max, iPhone 12 Pro Max
+        XCTAssertEqual(CGSize.iPhone13ProMax.height, 926)
+    }
+}
+
--- Tests/ScreenshottingTests/DeviceStaticInstancesTests.swift
+++ Tests/ScreenshottingTests/DeviceStaticInstancesTests.swift
@@ -44,6 +44,39 @@ final class DeviceStaticInstancesTests: XCTestCase {
         XCTAssertEqual(Device.iPhone13ProMax.name, "iPhone 13 Pro Max")
     }
     
+    func test_device_size() {
+        /// `4.7"` (375 x 667 points @2x)
+        /// iPhone SE (2nd Gen), iPhone 8, iPhone 7, iPhone 6S
+        XCTAssertEqual(Device.iPhoneSE2nd.size, CGSize.iPhoneSE2nd)
+        
+        /// `5.4"` (375 x 812 points @3x)
+        /// iPhone 13 mini, iPhone 12 mini
+        XCTAssertEqual(Device.iPhone13mini.size, CGSize.iPhone13mini)
+        
+        /// `5.5"` (414 x 736 points @3x)
+        /// iPhone 8 Plus, iPhone 7 Plus, iPhone 6S Plus
+        XCTAssertEqual(Device.iPhone8Plus.size, CGSize.iPhone8Plus)
+        
+        /// `5.8"` (375 x 812 points @3x)
+        /// iPhone 11 Pro, iPhone XS, iPhone X
+        XCTAssertEqual(Device.iPhoneX.size, CGSize.iPhoneX)
+        
+        /// `6.1"` (414 x 896 points @2x)
+        /// iPhone 11, iPhone XR
+        XCTAssertEqual(Device.iPhone11.size, CGSize.iPhone11)
+        
+        /// `6.1"` (390 x 844 points @3x)
+        /// iPhone 13 Pro, iPhone 13, iPhone 12 Pro, iPhone 12
+        XCTAssertEqual(Device.iPhone13Pro.size, CGSize.iPhone13Pro)
+        
+        /// `6.5"` (414 x 896 points @3x)
+        /// iPhone 11 Pro Max, iPhone XS Max
+        XCTAssertEqual(Device.iPhoneXSMax.size, CGSize.iPhoneXSMax)
+        
+        /// `6.7"` (428 x 926 points @3x)
+        /// iPhone 13 Pro Max, iPhone 12 Pro Max
+        XCTAssertEqual(Device.iPhone13ProMax.size, CGSize.iPhone13ProMax)
+    }
     
     func test_device_scale() {
         /// `4.7"` (375 x 667 points @2x)
@@ -78,73 +111,4 @@ final class DeviceStaticInstancesTests: XCTestCase {
         /// iPhone 13 Pro Max, iPhone 12 Pro Max
         XCTAssertEqual(Device.iPhone13ProMax.scale, 3)
     }
-    
-    func test_device_width() {
-        /// `4.7"` (375 x 667 points @2x)
-        /// iPhone SE (2nd Gen), iPhone 8, iPhone 7, iPhone 6S
-        XCTAssertEqual(Device.iPhoneSE2nd.size.width, 375)
-        
-        /// `5.4"` (375 x 812 points @3x)
-        /// iPhone 13 mini, iPhone 12 mini
-        XCTAssertEqual(Device.iPhone13mini.size.width, 375)
-        
-        /// `5.5"` (414 x 736 points @3x)
-        /// iPhone 8 Plus, iPhone 7 Plus, iPhone 6S Plus
-        XCTAssertEqual(Device.iPhone8Plus.size.width, 414)
-        
-        /// `5.8"` (375 x 812 points @3x)
-        /// iPhone 11 Pro, iPhone XS, iPhone X
-        XCTAssertEqual(Device.iPhoneX.size.width, 375)
-        
-        /// `6.1"` (414 x 896 points @2x)
-        /// iPhone 11, iPhone XR
-        XCTAssertEqual(Device.iPhone11.size.width, 414)
-        
-        /// `6.1"` (390 x 844 points @3x)
-        /// iPhone 13 Pro, iPhone 13, iPhone 12 Pro, iPhone 12
-        XCTAssertEqual(Device.iPhone13Pro.size.width, 390)
-        
-        /// `6.5"` (414 x 896 points @3x)
-        /// iPhone 11 Pro Max, iPhone XS Max
-        XCTAssertEqual(Device.iPhoneXSMax.size.width, 414)
-        
-        /// `6.7"` (428 x 926 points @3x)
-        /// iPhone 13 Pro Max, iPhone 12 Pro Max
-        XCTAssertEqual(Device.iPhone13ProMax.size.width, 428)
-    }
-    
-    func test_device_height() {
-        /// `4.7"` (375 x 667 points @2x)
-        /// iPhone SE (2nd Gen), iPhone 8, iPhone 7, iPhone 6S
-        XCTAssertEqual(Device.iPhoneSE2nd.size.height, 667)
-        
-        /// `5.4"` (375 x 812 points @3x)
-        /// iPhone 13 mini, iPhone 12 mini
-        XCTAssertEqual(Device.iPhone13mini.size.height, 812)
-        
-        /// `5.5"` (414 x 736 points @3x)
-        /// iPhone 8 Plus, iPhone 7 Plus, iPhone 6S Plus
-        XCTAssertEqual(Device.iPhone8Plus.size.height, 736)
-        
-        /// `5.8"` (375 x 812 points @3x)
-        /// iPhone 11 Pro, iPhone XS, iPhone X
-        XCTAssertEqual(Device.iPhoneX.size.height, 812)
-        
-        /// `6.1"` (414 x 896 points @2x)
-        /// iPhone 11, iPhone XR
-        XCTAssertEqual(Device.iPhone11.size.height, 896)
-        
-        /// `6.1"` (390 x 844 points @3x)
-        /// iPhone 13 Pro, iPhone 13, iPhone 12 Pro, iPhone 12
-        XCTAssertEqual(Device.iPhone13Pro.size.height, 844)
-        
-        /// `6.5"` (414 x 896 points @3x)
-        /// iPhone 11 Pro Max, iPhone XS Max
-        XCTAssertEqual(Device.iPhoneXSMax.size.height, 896)
-        
-        /// `6.7"` (428 x 926 points @3x)
-        /// iPhone 13 Pro Max, iPhone 12 Pro Max
-        XCTAssertEqual(Device.iPhone13ProMax.size.height, 926)
-    }
 }
-
